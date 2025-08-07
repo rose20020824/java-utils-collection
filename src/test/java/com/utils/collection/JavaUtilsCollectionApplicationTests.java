d44ff913d8e24aa475338c4a72b3eed22fa38e50
lzj <2632804487@qq.com>
2025-08-07 18:07:17 +0800

    初始化工具类项目，添加基础结构

---
+++ src/test/java/com/utils/collection/JavaUtilsCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utils.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilsCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
