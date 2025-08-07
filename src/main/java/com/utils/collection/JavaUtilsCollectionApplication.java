513c8e63a9b46c57ba60674d187afef09c6a9c17
lzj <2632804487@qq.com>
2025-08-07 18:11:03 +0800

    初始化

--- src/main/java/com/utils/collection/JavaUtilsCollectionApplication.java
+++ src/main/java/com/utils/collection/JavaUtilsCollectionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaUtilsCollectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaUtilsCollectionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUtilsCollectionApplication.class, args);
+    }
 
 }
